a4840d610c7b6f48e901e04a7e6c0cf4ef23fc35
Sh8hab <shehabhoba11@gmail.com>
2023-03-12 19:38:48 +0200

    first commit

--- lib/home.dart
+++ lib/home.dart
@@ -27,65 +27,84 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           elevation: 0.0,
           centerTitle: true,
-          title: const Text("NOTE APP",style: TextStyle(
-              color: Colors.white,fontSize: 35,fontStyle: FontStyle.italic,
-              letterSpacing: 4),),
-
+          title: const Text(
+            "NOTE APP",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 35,
+                fontStyle: FontStyle.italic,
+                letterSpacing: 4),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
             backgroundColor: Colors.white,
-            child: const Icon(Icons.add,color: Colors.red,size: 35,),
+            child: const Icon(
+              Icons.add,
+              color: Colors.red,
+              size: 35,
+            ),
             onPressed: () {
-              showDialog(context: context, builder: (BuildContext context){
-                return AlertDialog(
-                  title: const Text("Add Todo"),
-                  content: TextField(
-                    decoration: const InputDecoration(
-                        hintText: "To do's"),
-                    onChanged: (String valu){
-                      input= valu;
-                    },
-                  ),
-                  actions: [TextButton(onPressed: (){
-                    setState(() {
-                      todo.add(input);
-                    });
-                    Navigator.of(context).pop();
-
-                  }
-                      , child: const Text("Add ",style: TextStyle(color: Colors.black),)
-                  )
-                  ],
-                );
-              });
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text("Add Todo"),
+                      content: TextField(
+                        decoration: const InputDecoration(hintText: "To do's"),
+                        onChanged: (String valu) {
+                          input = valu;
+                        },
+                      ),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              setState(() {
+                                todo.add(input);
+                              });
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text(
+                              "Add ",
+                              style: TextStyle(color: Colors.black),
+                            ))
+                      ],
+                    );
+                  });
             }),
         body: Padding(
           padding: const EdgeInsets.all(5),
           child: ListView.builder(
               itemCount: todo.length,
-              itemBuilder: (BuildContext contect , int index){
-                return Dismissible(key: Key(todo[index]), child: Card(
-                  elevation: 4,
-                  margin: const EdgeInsets.all(8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: ListTile(
-                    title: Text(todo[index],style: const TextStyle(color: Colors.black),),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete,color: Colors.red,),
-                      onPressed: () {
-                        setState(() {
-                          todo.removeAt(index);
-                        });
-                      },
-                    ),
-                  ),
-                ));
-              }
-
-          ),
+              itemBuilder: (BuildContext contect, int index) {
+                return Dismissible(
+                    key: Key(todo[index]),
+                    child: Card(
+                      elevation: 4,
+                      margin: const EdgeInsets.all(8),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: ListTile(
+                        title: Text(
+                          todo[index],
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                        trailing: IconButton(
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              todo.removeAt(index);
+                            });
+                          },
+                        ),
+                      ),
+                    ));
+              }),
         ),
       ),
-    );}
+    );
+  }
 }
